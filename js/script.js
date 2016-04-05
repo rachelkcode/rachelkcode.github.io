a0a83e536713e99c148741d604af8db72dd60903
rachelkcode <Rachel@Rachels-MacBook-Air.local>
2016-04-05 18:40:22 -0400

    Final project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $( '#container' ).SecretNav({       
+        navSelector: 'nav',         // selector of the nav tag
+        openSelector: '.open-menu', // selector of the menu's opener
+        position: 'left'            // left | top
+    });
+});
+
+
